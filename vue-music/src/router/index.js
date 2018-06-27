4bf2c0b89e15e4ff7e74f71a79e475161c0ef270
nhclike <2471814041@qq.com>
2018-06-27 10:55:38 +0800

    测试样式引入

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Test from '@/components/test'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 })
