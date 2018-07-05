1549bd2a9eaec3301b7b198199dbfec2de34b4e6
nhclike <2471814041@qq.com>
2018-07-05 11:25:33 +0800

    slider组件开发完成

--- vue-music/src/base/slider/slider.vue
+++ vue-music/src/base/slider/slider.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <div class="slider">
-      <div class="slider-group">
+    <div class="slider" ref="slider">
+      <div class="slider-group" ref="sliderGroup">
         <slot></slot>
       </div>
     </div>
     <div class="dots">
-
+      <span class="dot" v-for="(item,index) in dots"  :class="{active:index==currentPageIndex}"></span>
     </div>
   </div>
 </template>
 
 <script >
+  import {addClass} from '@/common/js/dom'
+  import BScroll from 'better-scroll'
   export default {
+    name:'slider',
     props:{
       loop:{
         type:Boolean,
@@ -28,9 +31,84 @@
       }
     },
     data(){
-      return {}
+      return {
+        dots:[],
+        currentPageIndex:0
+      }
+    },
+    mounted(){
+      setTimeout(()=>{
+        this._setSliderWidth();
+        this._initDots();
+        this._initSlider();
+        if(this.autoPlay){
+          this._play();
+        }
+      },20)
+      window.addEventListener('resize',()=>{
+        if(!this.slider){
+          return
+        }
+        this._setSliderWidth(true);
+      })
+    },
+    methods:{
+      _setSliderWidth(isResize){
+        this.children=this.$refs.sliderGroup.children;
+        /*console.log(this);
+        console.log(this.loop);*/
+        let width=0;
+        let sliderWidth=this.$refs.slider.clientWidth;
+        for(let i=0;i<this.children.length;i++){
+          let child=this.children[i];
+          addClass(child,'slider-item');
+          child.style.width=sliderWidth+'px';
+          width+=sliderWidth;
+        }
+        if(this.loop && !isResize){
+         width +=2* sliderWidth;
+        }
+        this.$refs.sliderGroup.style.width=width+'px';
+      },
+      _initDots(){
+        this.dots=new Array(this.children.length);
+      },
+      _initSlider(){
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        });
+        this.slider.on('scrollEnd',()=>{
+          let pageIndex=this.slider.getCurrentPage().pageX;
+          console.log(pageIndex+'pageIndex');
+          if(this.loop){
+            pageIndex-=1;
+          }
+          this.currentPageIndex=pageIndex;
+          if(this.autoPlay){
+            clearTimeout(this.timer);
+            this._play();
+          }
+        })
+      },
+      _play(){
+        let pageIndex=this.currentPageIndex+1;
+        if(this.loop){
+          pageIndex+=1;
+        }
+        this.timer=setTimeout(()=>{
+          this.slider.goToPage(pageIndex,0 ,400)
+        },this.interval)
+      }
+    },
+    destroyed(){
+      clearTimeout(this.timer);
     }
-
   }
 </script>
 
@@ -42,24 +120,45 @@
       position: relative;
       overflow: hidden;
       white-space: nowrap;
-      a{
-        display: block;
-        width: 100%;
+      .slider-item{
+        float: left;
+        box-sizing: border-box;
         overflow: hidden;
-        text-decoration: none;
-        img{
-          display: block;
-          width: 100%;
+        text-align: center;
+          a{
+            display: block;
+            width: 100%;
+            overflow: hidden;
+            text-decoration: none;
+          img{
+            display: block;
+            width: 100%;
+          }
         }
       }
     }
-    .dots{
-      position: absolute;
-      right: 0;
-      left: 0;
-      bottom: 12px;
-      text-align: center;
-      font-size: 0;
+  }
+  .dots{
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 12px;
+    text-align: center;
+    font-size: 0;
+    .dot{
+      display: inline-block;
+      margin: 0 4px;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background:@color-text-l;
+      &.active{
+         width: 10px;
+         height: 10px;
+
+         border-radius: 5px;
+         background:@color-text-ll;
+      }
     }
   }
 </style>
--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="recommend">
    <div class="recommend-content">
-     <div class="slider-wrapper">
+     <div class="slider-wrapper" v-if="recommends.length">
         <slider>
           <div v-for="item in recommends">
             <a :href="item.linkUrl">
