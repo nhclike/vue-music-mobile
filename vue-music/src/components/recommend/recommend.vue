ed1c2adab7ba9ddca5556c0a42054b0e595e959d
nhclike <2471814041@qq.com>
2018-07-09 17:11:47 +0800

    歌曲列表渲染

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -13,7 +13,15 @@
      <div class="recommend-list">
        <h1 class="list-title">热门歌单推荐</h1>
        <ul>
-
+          <li v-for="item in discList" class="item">
+            <div class="icon">
+              <img width="60" height="60"  :src="item.imgurl">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
        </ul>
      </div>
    </div>
@@ -51,6 +59,7 @@
         getDiscList().then((res)=>{
           if(res.code===ERR_OK){
             console.log(res.data.list);
+            this.discList=res.data.list;
           }
         })
       }
@@ -81,6 +90,35 @@
           text-align: center;
           font-size:@font-size-medium;
           color:@color-theme;
+        }
+        ul{
+          .item{
+            display: flex;
+            box-sizing: border-box;
+            align-items: center;
+            padding: 0 20px 20px 20px;
+            .icon{
+              flex: 0 0 60px;
+              width: 60px;
+              padding-right: 20px;
+            }
+            .text{
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              flex: 1;
+              line-height: 20px;
+              overflow: hidden;
+              font-size:@font-size-medium;
+              .name{
+                margin-bottom: 10px;
+                color:@color-text
+              }
+              .desc{
+                color:@color-text-d
+              }
+            }
+          }
         }
       }
     }
