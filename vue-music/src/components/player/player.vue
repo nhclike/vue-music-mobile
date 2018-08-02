59a93979f0009dc75d770fa6838353a82acdabe2
nhclike <2471814041@qq.com>
2018-08-02 15:09:26 +0800

    增加create-keyframe-animation的使用，优化播放页面动画效果

--- vue-music/src/components/player/player.vue
+++ vue-music/src/components/player/player.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="player" v-show="playList.length>0">
-    <transition name="normal">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
       <div class="normal-player" v-show="fullScreen">
         <div class="background">
           <img :src="currentSong.image" alt="" width="100%" height="100%">
@@ -14,7 +19,7 @@
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div class="cd-wrapper">
+            <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd">
                 <img :src="currentSong.image" class="image" alt="">
 
@@ -72,6 +77,9 @@
 <script type="text/ecmascript-6">
   import {mapGetters,mapMutations} from 'vuex';
   import animations from 'create-keyframe-animation';
+  import {prefixStyle} from '@/common/js/dom';
+  const transform = prefixStyle('transform');
+  const transitionDuration = prefixStyle('transitionDuration');
   export default {
     data(){
       return {}
@@ -105,6 +113,61 @@
       },
       open(){
         this.setFullScreen(true);
+      },
+      enter(el, done) { //利用钩子函数动态操作动画
+        /*const {x, y, scale} = this._getPosAndScale();
+
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        };
+
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        });
+
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)*/
+      },
+      afterEnter() {
+        /*animations.unregisterAnimation('move');
+        this.$refs.cdWrapper.style.animation = ''*/
+      },
+      leave(el, done) {
+        /*this.$refs.cdWrapper.style.transition = 'all 0.4s';
+        const {x, y, scale} = this._getPosAndScale();
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`;
+        this.$refs.cdWrapper.addEventListener('transitionend', done)*/
+      },
+      afterLeave() {
+        /*this.$refs.cdWrapper.style.transition = '';
+        this.$refs.cdWrapper.style[transform] = '';*/
+      },
+      _getPosAndScale() {
+        const targetWidth = 40;
+        const paddingLeft = 40;
+        const paddingBottom = 30;
+        const paddingTop = 80;
+        const width = window.innerWidth * 0.8;
+        const scale = targetWidth / width;
+        const x = -(window.innerWidth / 2 - paddingLeft);
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom;
+        return {
+          x,
+          y,
+          scale
+        }
       },
       ...mapMutations( //不能直接修改vuex中的变量,通过映射方法传参数的方式提交改变vuex中的参数
         {
