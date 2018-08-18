cb82ae99f5f012d057acbc20eceb8ff59342c19c
nhclike <nhc201108>
2018-08-18 10:14:24 +0800

    搜索框样式基本完成

--- vue-music/src/components/search/search.vue
+++ vue-music/src/components/search/search.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <h1>搜索页面</h1>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
+
   </div>
 </template>
 
 <script  type="text/ecmascript-6">
+  import SearchBox from '@/base/search-box/search-box'
   export default {
     data(){
       return {}
+    },
+    components:{
+      SearchBox
     }
   }
 </script>
 
 <style scoped lang="less">
+  @import "./../../common/css/variable";
   h1{
     color:#fff
+  }
+  .search{
+    position: fixed;
+    top:80px;
+    bottom: 0;
+    width: 100%;
   }
 </style>
